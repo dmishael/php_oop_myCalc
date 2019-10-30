7489d6243293e35ff97589bf01d4b76e3e77a07a
daniel mishael <dmishael91@gmail.com>
2019-10-30 16:26:36 -0400

    finished calculator

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    declare(strict_types = 1);
+    include 'includes/class-autoload.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="includes/calc.inc.php" method="post">
+        <p>My Calculator</p>
+        <input type="number" name="num1" placeholder="First number">
+        
+        <select name="oper">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select>
+
+        <input type="number" name="num2" placeholder="Second number">
+
+        <button type="submit" value="Submit" name="submit">Calculate</button>
+    </form>
+
+</body>
+</html>
